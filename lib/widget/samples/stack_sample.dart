2ee23dd0944852e2fa8a2035dc48e1992bb4400d
DragonCherry <dragoncherry@naver.com>
2020-10-22 16:39:51 +0900

    added stack sample

--- lib/widget/samples/stack_sample.dart
+++ lib/widget/samples/stack_sample.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toolset/toolset.dart';
 
 class StackSample extends StatefulWidget {
   @override
@@ -13,31 +14,35 @@ class _StackSampleState extends State<StackSample> {
 
   @override
   Widget build(BuildContext context) {
-    final body = GridView.count(crossAxisCount: 2, children: [
-      Stack(children: [
-        Container(color: Colors.white),
-        Opacity(
-            opacity: 0.5,
-            child: Container(
-              decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.all(const Radius.circular(30))),
-              child: Center(child: CircularProgressIndicator()),
-            ))
-      ]),
-      Stack(children: [
-        Container(color: Colors.white),
-        Opacity(
-            opacity: 0.5,
-            child: Container(
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.all(const Radius.circular(30))),
-              child: Center(child: CircularProgressIndicator()),
-            ))
-      ])
-    ]);
+    final width = MediaQuery.of(context).size.width;
+    final body = GridView.count(
+        crossAxisCount: 1,
+        children: [firstStackSample(), secondStackSample(width)]);
     return Scaffold(
         appBar: AppBar(title: Text('${widget.runtimeType}')), body: body);
+  }
+
+  Stack firstStackSample() {
+    return Stack(children: [
+      Container(color: Colors.white),
+      Opacity(
+          opacity: 0.5,
+          child: Container(
+            decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.all(const Radius.circular(30))),
+            child: Center(child: CircularProgressIndicator()),
+          ))
+    ]);
+  }
+
+  Stack secondStackSample(final double width) {
+    final diff = width / Colors.primaries.length;
+    final children = Colors.primaries
+        .enumerated((i, e) => Center(
+            child: Container(
+                width: width - diff * i, height: width - diff * i, color: e)))
+        .toList();
+    return Stack(children: children);
   }
 }
